8791eb104b384dd8f4ccee0314fafa15703e8591
mlngwan <david027301@naver.com>
2024-04-06 00:03:54 +0900

    :memo: 접근 제어 변경

--- src/main/java/com/mentoring/mju/v1/racing/view/InputView.java
+++ src/main/java/com/mentoring/mju/v1/racing/view/InputView.java
@@ -8,15 +8,15 @@ import java.util.SplittableRandom;
 public class InputView {
 
     // Todo : 입력 받기
-    private static final Scanner input = new Scanner(System.in);
+    public static final Scanner input = new Scanner(System.in);
 
-    private List<String> getCarName(){
+    public List<String> getCarName(){
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String CarName = input.nextLine();
         return Arrays.asList(CarName.split(","));
     }
 
-    private int getTryNumber(){
+    public int getTryNumber(){
         System.out.println("시도할 횟수는 몇회인가요?");
         return input.nextInt();
     }
