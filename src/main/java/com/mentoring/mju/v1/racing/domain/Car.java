996dee7dbf26e28310dd18e076b06ca0a54f8243
mlngwan <david027301@naver.com>
2024-04-06 00:03:36 +0900

    :memo: 0 ~ 9 Random Number 생성 및 4 이상 생성시 distance 1 증가 method 구현

--- src/main/java/com/mentoring/mju/v1/racing/domain/Car.java
+++ src/main/java/com/mentoring/mju/v1/racing/domain/Car.java
@@ -1,15 +1,4 @@
 package com.mentoring.mju.v1.racing.domain;
 
-import java.util.Random;
-
 public class Car {
-    Random random = new Random();
-    int distance = 0;
-
-    public void carShouldGo(){
-        int randNum = random.nextInt(10);
-        if(randNum >= 4){
-            this.distance++;
-        }
-    }
 }
